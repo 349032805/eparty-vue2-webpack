6176ff06a1de007d8098ac4e3d1252aeea2b0665
349032805@qq.com <349032805@qq.com>
2017-04-04 15:18:11 +0800

    git commit

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -5,15 +5,18 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: './src/main.js'
-  },
+  // entry: {
+  //   vendor: ["vue", "vue-resource","vue-router","babel-runtime","better-scroll","vuex"],
+  //   app: "./src/main.js"
+  // },
   output: {
     path: __dirname + '/build',
     // path: path.join(__dirname, 'build'),
     // publicPath: '', //资源路径前缀
-    filename: "scripts/[name]-[chunkhash:8].js"
+    filename: "scripts/[name]-[chunkhash:8].js",
+    chunkFilename: "scripts/[id]-[chunkhash:8].js"
   },
+
   resolve: {
     extensions: ['', '.js', '.vue'],
     fallback: [path.join(__dirname, 'node_modules')],
@@ -99,27 +102,57 @@ module.exports = {
       })
     ]
   },
-	devtool: 'cheap-source-map',
 	plugins: [
 		new CleanWebpackPlugin('build', {
 	      verbose: true,
 	      dry: false
-	    }),
+    }),
 		new ExtractTextPlugin("styles/[name]-[contenthash:8].css"),
 		new webpack.optimize.DedupePlugin(),
-	    new webpack.optimize.OccurrenceOrderPlugin(),
-	    new HtmlWebpackPlugin({
-	      filename: 'index.html',
-	      template: 'index.html',
-	      inject: true
-	    }),
-	    //压缩
-	    new webpack.optimize.UglifyJsPlugin({
-	        compress: {
-	            warnings: false,
-	            drop_console: true
-	        },
-	        comments: false,
-	    })
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+        // more options:
+        // https://github.com/kangax/html-minifier#options-quick-reference
+      },
+      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+      chunksSortMode: 'dependency'
+    }),
+    //压缩
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false,
+            drop_console: true
+        },
+        comments: false,
+        sourceMap: false
+    }),
+    // split vendor js into its own file
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function (module, count) {
+        // any required modules inside node_modules are extracted to vendor
+        return (
+          module.resource &&
+          /\.js$/.test(module.resource) &&
+          module.resource.indexOf(
+            path.join(__dirname, '../node_modules')
+          ) === 0
+        )
+      }
+    }),
+    // extract webpack runtime and module manifest to its own file in order to
+    // prevent vendor hash from being updated whenever app bundle is updated
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest',
+      chunks: ['vendor']
+    })
+
 	]
 }
